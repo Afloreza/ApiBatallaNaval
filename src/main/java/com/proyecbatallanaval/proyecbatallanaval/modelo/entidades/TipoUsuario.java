08611570f235f952c70540043335672e4f72c8a3
AngelaFlorez <amflorez80793@umanizales.edu.co>
2021-06-04 15:16:03 -0500

    tipoUsuario gestionado

---
+++ src/main/java/com/proyecbatallanaval/proyecbatallanaval/modelo/entidades/TipoUsuario.java
@@ -0,0 +1,56 @@
+package com.proyecbatallanaval.proyecbatallanaval.modelo.entidades;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tipo_usuario", schema = "public", catalog = "batalla_naval")
+//Clase que permite administrar los tipos de usuario,
+// 1 - Jugador
+// 2 - Adminsitrador
+
+public class TipoUsuario {
+    private short codigo;
+    private String descripcion;
+
+    @Id
+    @Column(name = "codigo", nullable = false)
+    public short getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(short codigo) {
+        this.codigo = codigo;
+    }
+
+    @Basic
+    @Column(name = "descripcion", nullable = false, length = 35)
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TipoUsuario that = (TipoUsuario) o;
+
+        if (codigo != that.codigo) return false;
+        if (descripcion != null ? !descripcion.equals(that.descripcion) : that.descripcion != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) codigo;
+        result = 31 * result + (descripcion != null ? descripcion.hashCode() : 0);
+        return result;
+    }
+
+}
